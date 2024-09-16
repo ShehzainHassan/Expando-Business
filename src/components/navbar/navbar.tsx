7a4573e4cf2bdc4588809dba8c600dc559314c67
Shehzain Hassan <shehzainhassan@gmail.com>
2024-09-16 12:39:30 +0500

    Added Hamburger menu for mobile layout

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,13 +1,49 @@
+"use client";
+import { useEffect, useRef, useState } from "react";
 import classes from "./navbar.module.css";
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [menuRef]);
   return (
-    <div className={classes.navbar}>
-      <a href="#home">Home</a>
-      <a href="#about">About Us</a>
-      <a href="#services">Services</a>
-      <a href="#services-2">Services</a>
-      <a href="#feedback">Feedback</a>
-      <a href="#contact">Contact</a>
-    </div>
+    <>
+      <div className={classes.navbar}>
+        <button onClick={toggleMenu} className={classes.menuButton}>
+          <img src="/img/hamburger.svg" alt="hamburger"></img>
+        </button>
+        <a href="#home">Home</a>
+        <a href="#about">About Us</a>
+        <a href="#services">Services</a>
+        <a href="#services-2">Services</a>
+        <a href="#feedback">Feedback</a>
+        <a href="#contact">Contact</a>
+      </div>
+
+      {isMenuOpen && (
+        <div className={classes.menu} ref={menuRef}>
+          <a href="#home">Home</a>
+          <a href="#about">About Us</a>
+          <a href="#services">Services</a>
+          <a href="#services-2">Services</a>
+          <a href="#feedback">Feedback</a>
+          <a href="#contact">Contact</a>
+        </div>
+      )}
+    </>
   );
 }
